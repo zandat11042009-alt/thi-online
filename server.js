6f77869ce9a7413c8c12e295ece17237bb155dc6
zandat11042009-alt <zandat11042009@gmail.com>
2026-05-01 12:53:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 app.use(express.static("public"));
+
+let questions = []; // lưu tạm (RAM)
+
+app.post("/add-question", (req, res) => {
+    questions.push(req.body);
+    res.json({ message: "Đã thêm câu hỏi" });
+});
+
+app.get("/questions", (req, res) => {
+    res.json(questions);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server chạy cổng " + PORT));
